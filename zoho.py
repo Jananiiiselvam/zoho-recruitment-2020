413492e06b63a12dd28eb6f1025e1035e00ad532
Jananiiiselvam <86999581+Jananiiiselvam@users.noreply.github.com>
2021-07-06 10:37:40 +0530

    Add files via upload

---
+++ zoho.py
@@ -0,0 +1,17 @@
+a = 'welcome'
+mid = int(len(a)//2)
+str1 = a[mid:]
+str2 = a[:mid]
+end_string = str1+str2
+k = 2*len(a) - 2
+for i in range(0, len(a)+1):
+  for j in range(0, k):
+      print(end=" ")
+  k = k - 2
+
+  for j in range(0, i+1):
+      output = end_string[:i]
+      print(output , end="")
+      break
+    
+  print("\r")
